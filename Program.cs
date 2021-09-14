92cfcfc52ce6b2e0ad493d37f4a8e5e00f5788d1
Itamar Junior <itjunior@gmail.com>
2021-09-14 15:45:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Y2S1Week1Object_OrientedProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Itamar Junior";
+            int myAge = 15;
+            Console.WriteLine($"Hello {myName}, you're {myAge} years old.");
+        }
+    }
+}
